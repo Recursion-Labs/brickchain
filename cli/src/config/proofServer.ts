0e633345889f31abe5211257cd9d8d6d818de69e
samarth3301 <samarplayz1337@gmail.com>
2025-09-27 05:24:58 +0530

    Refactor proofServer configuration and add indexer settings; remove unused compact file and update package.json scripts

--- cli/src/config/proofServer.ts
+++ cli/src/config/proofServer.ts
@@ -1,15 +1,20 @@
-import path from "node:path";
+import path from "path";
+
 import { NetworkId, setNetworkId } from '@midnight-ntwrk/midnight-js-network-id';
 
 export const currentDir = path.resolve(new URL(import.meta.url).pathname, '..');
 export interface Config {
   readonly logDir: string;
+  readonly indexer: string;
+  readonly indexerWS: string;
   readonly node: string;
   readonly proofServer: string;
 }
 
 export class TestnetLocalConfig implements Config {
   logDir = path.resolve(currentDir, '..', 'logs', 'testnet-local', `${new Date().toISOString()}.log`);
+  indexer = 'http://127.0.0.1:8088/api/v1/graphql';
+  indexerWS = 'ws://127.0.0.1:8088/api/v1/graphql/ws';
   node = 'http://127.0.0.1:9944';
   proofServer = 'http://127.0.0.1:6300';
   constructor() {
