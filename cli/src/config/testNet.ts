2163e0367ed5df25e9004a7d464ba7a67c9a66b3
samarth3301 <samarplayz1337@gmail.com>
2025-09-28 16:39:54 +0530

    fix: update testnet configuration URLs for consistency and accuracy

--- cli/src/config/testNet.ts
+++ cli/src/config/testNet.ts
@@ -6,10 +6,10 @@ export class TestnetRemoteConfig implements Config {
   privateStateStoreName = 'real-estate-token-private-state';
   logDir = path.resolve(currentDir, '..', 'logs', 'testnet-remote', `${new Date().toISOString()}.log`);
   zkConfigPath = path.resolve(currentDir, '..', '..', '..', 'contracts', 'managed', 'main');
-  indexer = 'https://indexer.testnet-02.midnight.network/api/v1/graphql';
-  indexerWS = 'wss://indexer.testnet-02.midnight.network/api/v1/graphql/ws';
-  node = 'https://rpc.testnet-02.midnight.network';
-  proofServer = 'http://127.0.0.1:6300';
+  indexer = 'https://indexer.testnet.midnight.network/api/v1/graphql';
+  indexerWS = 'wss://indexer.testnet.midnight.network/api/v1/graphql/ws';
+  node = 'https://rpc.testnet.midnight.network';
+  proofServer = 'https://proof-server.testnet.midnight.network';
 
   setNetworkId() {
     setNetworkId(NetworkId.TestNet);
